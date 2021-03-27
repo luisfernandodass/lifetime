beda211ac3a2db537c18f1ee574a2630b999ad8b
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-03-27 16:07:11 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*
 const button = document.getElementById("research");
 
 button.addEventListener("click", function(){
@@ -17,3 +17,20 @@ button.addEventListener("click", function(){
     elMsg.textContent = Math.floor(difference) + ' dias';
     
 });
+*/
+const button = document.getElementById("research");
+
+function fat(button){
+    const getInput = document.getElementById("getInput");
+
+    const value = getInput.value;
+
+    var today = new Date();
+    var dataStart = new Date(value);
+
+    var difference =  today.getTime() - dataStart.getTime();
+    difference = (difference / 86400000);
+
+    var elMsg = document.getElementById("box-quantity-days");
+    return elMsg.textContent = Math.floor(difference) + ' dias';
+}
