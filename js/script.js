9977226be10bd94bf1bdae497077dbc257a6119d
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-04-28 14:17:25 -0300

    imprime a data de nascimento e atual

--- js/script.js
+++ js/script.js
@@ -3,25 +3,26 @@ const button = document.getElementById("toCount");
 
 button.addEventListener("click", function(){
 
-    var day = document.getElementById("day").value;
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-
+    var inputDay = document.getElementById("day").value;
+    var inputMonth = document.getElementById("month").value;
+    var inputYear = document.getElementById("year").value;
     
-    var currentDate = new Date();                                   // Pega a data atual
-   
-    var birthDate = new Date(year, month, day);                     // Pega a data de nascimento
+    var currentDate = new Date();                                   // Pega a data atual   
+    var inputBirthDate = new Date(inputYear, inputMonth, inputDay);                     // Pega a data de nascimento
                       
-    var difference =  currentDate.getTime() - birthDate.getTime();  // Subtrai em millisegundos a data de nascimento da data atual
-    difference = (difference / 86400000) + 30 ;                     // 864000000 = 1 dia em millisegundos
+    var difference =  currentDate.getTime() - inputBirthDate.getTime();  // Subtrai em millisegundos a data de nascimento da data atual
+    difference = (difference / 86400000) + 30;                     // 864000000 = 1 dia em millisegundos
 
-    var elMsg = document.getElementById("result");
-    elMsg.textContent =  Math.floor(difference) + ' dias';           // Mostra resultado na tela
+    var result = document.getElementById("result");
+    result.textContent =  Math.floor(difference) + ' dias';           // Mostra resultado na tela
 
-    var firstBirthDate = document.getElementById("first-birthDate");
-    firstBirthDate.textContent  = 'Data de nascimento: ' + birthDate;
+    var printBirthDate = document.getElementById("printBirthDate");
+    printBirthDate.textContent  = 'Data de nascimento: ' + inputBirthDate.toLocaleDateString();
     
-    var a = document.getElementById("presentDate");
-    a.textContent = 'Data atual: ' + currentDate;
+    var printCurrentDate = document.getElementById("printCurrentDate");
+    printCurrentDate.textContent = 'Data atual: ' + currentDate.toLocaleDateString();
+
+    var trait = document.getElementById("trait");
+    trait.textContent = '- ' ;
 
 });
