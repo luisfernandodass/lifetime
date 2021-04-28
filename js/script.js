0448a88ececadfb4a4800c427dc07161abac5e0a
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-04-27 21:43:16 -0300

    foi mas está com uma diferença de - 30 dias

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
-/*
-const button = document.getElementById("research");
+const button = document.getElementById("toCount");
 
 button.addEventListener("click", function(){
 
-    const getInput = document.getElementById("getInput");
+    var day = document.getElementById("day").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
 
-    const value = getInput.value;
-
-    var today = new Date();
-    var dataStart = new Date(value);
-
-    var difference =  today.getTime() - dataStart.getTime();
-    difference = (difference / 86400000);
-
-    var elMsg = document.getElementById("box-quantity-days");
-    elMsg.textContent = Math.floor(difference) + ' dias';
     
+    var currentDate = new Date();  // Pega a data atual
+
+   
+    var birthDate = new Date(year, month, day); // Pega a data de nascimento
+                      
+    var difference =  currentDate.getTime() - birthDate.getTime();  // Subtrai em millisegundos a data de nascimento da data atual
+    difference = (difference / 86400000);                           // 864000000 = 1 dia em millisegundos
+
+    var elMsg = document.getElementById("result");
+    elMsg.textContent =  Math.floor(difference) + ' dias';
 });
-*/
+
+
+
+/*
 function toCount(){
     const getInput = document.getElementById("getInput");
 
@@ -32,3 +36,4 @@ function toCount(){
     var elMsg = document.getElementById("box-quantity-days");
     return elMsg.textContent = Math.floor(difference) + ' dias';
 }
+*/
