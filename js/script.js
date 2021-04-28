d609839c848875c5b82a029e54d043e271f18220
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-04-28 20:16:00 -0300

    update some validations on date

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ const button = document.getElementById("toCount");
 
 button.addEventListener("click", function(){
 
+  
     var inputDay = document.getElementById("day").value;
     var inputMonth = document.getElementById("month").value;
     var inputYear = document.getElementById("year").value;
-    
-    var currentDate = new Date();                                   // Pega a data atual   
-    var inputBirthDate = new Date(inputYear, inputMonth - 1, inputDay);                     // Pega a data de nascimento
+
+    if (inputDay > 0 & inputDay <= 31 & inputMonth > 0 & inputMonth <= 12 & inputYear.length <= 4){
+   
+    var currentDate = new Date();                                         // Pega a data atual   
+    var inputBirthDate = new Date(inputYear, inputMonth - 1, inputDay);   // Pega a data de nascimento
                       
     var difference =  currentDate.getTime() - inputBirthDate.getTime();  // Subtrai em millisegundos a data de nascimento da data atual
-    difference = (difference / 86400000);                     // 864000000 = 1 dia em millisegundos
+    difference = (difference / 86400000);                                // 864000000 = 1 dia em millisegundos
 
     var result = document.getElementById("result");
-    result.textContent =  Math.floor(difference) + ' dias';           // Mostra resultado na tela
+    result.textContent =  Math.floor(difference) + ' dias';             // Mostra resultado na tela
 
     var printBirthDate = document.getElementById("printBirthDate");
     printBirthDate.textContent  = 'Data de nascimento: ' + inputBirthDate.toLocaleDateString();
@@ -24,5 +27,10 @@ button.addEventListener("click", function(){
 
     var trait = document.getElementById("trait");
     trait.textContent = '- ' ;
+
+    } else {
+        var result = document.getElementById("result");
+        result.textContent =  'Data inválida'; 
+    }
 
 });
